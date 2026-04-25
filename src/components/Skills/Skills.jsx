a083c73250140c6ac5018ddc8dd165dd00a20b4f
Andy Vallejos <andyvallejosgb@gmail.com>
2026-04-24 23:54:00 -0400

    featur: change to Bento design

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,52 +1,36 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCss3Alt,
-  faHtml5,
-  faJs,
-  faNodeJs,
-  faReact,
-  faGitAlt,
-  faJava,
-  faPython,
-  faLinux,
-  faFigma,
-} from "@fortawesome/free-brands-svg-icons";
-
-import { faCode } from "@fortawesome/free-solid-svg-icons";
-
 import style from "./Skills.module.css";
-
-const MY_SKILLS = [
-  { name: "Html", icon: faHtml5, color: "#e34f26" },
-  { name: "Css", icon: faCss3Alt, color: "#1572b6" },
-  { name: "JavaScript", icon: faJs, color: "#f7df1e" },
-  { name: "Node.js", icon: faNodeJs, color: "#68a063" },
-  { name: "React", icon: faReact, color: "#61dafb" },
-  { name: "Git", icon: faGitAlt, color: "#f1502f" },
-  { name: "Java", icon: faJava, color: "#f89820" },
-  { name: "Python", icon: faPython, color: "#3776ab" },
-  { name: "C++", icon: faCode, color: "#00599c" },
-  { name: "Linux", icon: faLinux, color: "#000000" },
-  { name: "Figma", icon: faFigma, color: "#F24E1E" },
-];
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { skills, types } from "../../data/skills";
 
 export function Skills() {
   return (
     <section className={style.container}>
-      <h2>Skills</h2>
-      <section className={style.container__icons}>
-        <div className={style.iconGrid}>
-          {MY_SKILLS.map((skill, index) => (
-            <i
-              key={index}
-              className={style.skillLink}
-              style={{ "--skill-color": skill.color }}
-            >
-              <FontAwesomeIcon icon={skill.icon} />
-            </i>
-          ))}
-        </div>
-      </section>
+      <h2 className={style.title}>Skills</h2>
+
+      <div className={style.container__grid}>
+        {types.map((type) => (
+          <div
+            key={type}
+            className={`${style.iconGrid} ${style[`card__${type.toLowerCase()}`]}`}
+          >
+            <h3 className={style.typeLabel}>{type}</h3>
+            <div className={style.iconsWrapper}>
+              {skills
+                .filter((skill) => skill.type === type)
+                .map((skill, i) => (
+                  <a
+                    key={i}
+                    className={style.skillLink}
+                    href="#"
+                    style={{ "--hover-color": skill.color }}
+                  >
+                    <FontAwesomeIcon icon={skill.icon} />
+                  </a>
+                ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
